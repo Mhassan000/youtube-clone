4d21d38fe7c47a475ae9e6b75809e0dc959be90b
mhassan000 <hassanasif946@gmail.com>
2023-01-11 04:07:53 +0500

    Update Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import {Stack } from '@mui/material'
 import { categories } from '../utils/constant'
-const Sidebar = () => {
+const Sidebar = ({selectedCategory,setSelectedCategory}) => {
 
-    const selected = 'New';
     return (
         <Stack direction= 'row' 
         sx={{
@@ -13,8 +12,9 @@ const Sidebar = () => {
         }}>
         {categories.map((category) => (
             <button className='category-btn'
+            onClick={() => setSelectedCategory(category.name)}
             style={{
-                backgroundColor: selected === category.name && '#FC1503',
+                backgroundColor: selectedCategory === category.name && '#FC1503',
                 color: 'white',
                 
             }}
@@ -22,10 +22,10 @@ const Sidebar = () => {
             >
 
                 <span style={{
-                    color: category.name === selected ? 'white' : '#FC1503', marginRight: '15px'
+                    color: category.name === selectedCategory ? 'white' : '#FC1503', marginRight: '15px'
                 }}>{category.icon}</span>
                 <span style={{
-                    opacity: category.name === selected ? 1 : 0.8
+                    opacity: category.name === selectedCategory ? 1 : 0.8
                 }}>{category.name}</span>
             </button>
         ))}
