dab354128b9b8c50c2e8f6551c46c6a023972738
mhassan000 <hassanasif946@gmail.com>
2023-01-20 16:59:20 +0500

    Updated

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,6 @@ export { default as VideoDetail } from "./VideoDetail";
 export { default as ChannelDetails } from "./ChannelDetails";
 export { default as SearchDetails } from "./SearchDetails";
 export { default as Sidebar} from "./Sidebar";
+export { default as Videos} from "./Videos";
+export { default as VideoCard} from "./VideoCard";
+export { default as ChannelCard} from "./ChannelCard";  
